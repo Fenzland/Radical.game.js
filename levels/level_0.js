1b0fe0cfa90a9967b4823b287689726e8b6ba2ce
Fenz <uukoo@163.com>
2018-08-12 23:00:34 +0800

    add a new level_1

--- levels/level_0.js
+++ levels/level_0.js
@@ -23,4 +23,4 @@ export const goals= [
 	},
 ];
 
-export const is_last_level= true;
+export const is_last_level= false;
---
+++ levels/level_1.js
@@ -0,0 +1,58 @@
+export const title= '十一';
+
+export const comment= '過關條件：用“十”、“一”兩個漢字，拼出十一個不同的漢字。';
+
+export const unit_count= 2;
+
+export const units= [
+	'一', '十', 
+];
+
+export const goals= [
+	{
+		character: '二',
+		placeholder: '?',
+	},
+	{
+		character: '三',
+		placeholder: '?',
+	},
+	{
+		character: '亖',
+		placeholder: '?',
+	},
+	{
+		character: '土',
+		placeholder: '?',
+	},
+	{
+		character: '干',
+		placeholder: '?',
+	},
+	{
+		character: '王',
+		placeholder: '?',
+	},
+	{
+		character: '圭',
+		placeholder: '?',
+	},
+	{
+		character: '珪',
+		placeholder: '?',
+	},
+	{
+		character: '玨',
+		placeholder: '?',
+	},
+	{
+		character: '玕',
+		placeholder: '?',
+	},
+	{
+		character: '圩',
+		placeholder: '?',
+	},
+];
+
+export const is_last_level= true;
