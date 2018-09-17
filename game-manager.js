568cb99a8d4b40877dce8f32ab03cca31d142ba8
Fenz <uukooo@126.com>
2018-09-17 14:35:52 +0800

    styling code

--- game-manager.js
+++ game-manager.js
@@ -13,7 +13,7 @@ export default class GameManager
 		radical.addEventListener( 'round_over', e=> this.roundSettle(), );
 		radical.addEventListener( 'game_over', e=> this.gameOver(), );
 		
-		window.addEventListener( 'beforeunload', e=> this.store(), )
+		window.addEventListener( 'beforeunload', e=> this.store(), );
 		
 		this.states= new Model( {
 			level: 0,
